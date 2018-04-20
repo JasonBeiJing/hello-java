a7fb382fd3679341ff9db3483bc12dac6a96e395
wangshujie <coolday1103@126.com>
2018-04-20 10:32:53 +0800

    RENAME CLASS

--- src/main/java/com/hello/java/concurrent/ConcurrentExchanger.java
+++ src/main/java/com/hello/java/concurrent/ConcurrentExchanger.java
@@ -6,7 +6,7 @@ import java.util.concurrent.Exchanger;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class ConcurrentOfExchanger {
+public class ConcurrentExchanger {
 	private static final ExecutorService executor = Executors.newCachedThreadPool();
 	private static final Exchanger<List<String>> exchanger = new Exchanger<>();
 	
